5bbe9a3892648aa616847f1c8bb3b063b0b3cd88
Tim D <imacubsfan23@gmail.com>
2017-11-02 18:29:28 +0000

    Every 5th item comes up as SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |item, index|
+      item.title = "SPAM" if index % 5 == 0
+    end
   end
 
   def show
