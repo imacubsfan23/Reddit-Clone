9c00723a546024e331effa60c69054ee934f6d41
Tim D <imacubsfan23@gmail.com>
2017-11-07 21:22:58 +0000

    checkpoint 21 topics and posts completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 require 'random_data'
 
+15.times do 
+    Topic.create!(
+        name: RandomData.random_sentence,
+        description: RandomData.random_paragraph
+    )
+end
+topics = Topic.all
+
 50.times do
     Post.create!(
+        topic: topics.sample,
         title: RandomData.random_sentence,
         body:  RandomData.random_paragraph
     )
@@ -21,5 +30,6 @@ unique_post = Post.find_or_create_by!(title:"Unique Post Title", body:"Unique Po
 Comment.find_or_create_by!(post:unique_post, body:"Unique Comment Body")
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
