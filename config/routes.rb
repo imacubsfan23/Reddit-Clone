9f60a5eed07698627fe04a4f4cc0e13b9685a54f
Tim D <imacubsfan23@gmail.com>
2017-11-07 17:27:49 +0000

    edit function works correctly, but does not pass rspec test

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-
-  get 'questions/show'
-
-  get 'questions/new'
-
-  get 'questions/edit'
-
   resources :posts
   resources :questions
   
