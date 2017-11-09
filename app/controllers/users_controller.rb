20f8fe0ec08e9b5db76f8ff4daacc7f5341b661f
Tim D <imacubsfan23@gmail.com>
2017-11-09 18:27:13 +0000

    Checkpoint 24 authentication assignment completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
+    def confirm
+        @user = User.new
+        @user.name = params[:user].permit![:name]
+        @user.email = params[:user].permit![:email]
+        @user.password = params[:user].permit![:password]
+        @user.password_confirmation = params[:user].permit![:password_confirmation]
+    end
+    
     def create
         @user = User.new
-        @user.name = params[:user][:name]
-        @user.email = params[:user][:email]
-        @user.password = params[:user][:password]
-        @user.password_confirmation = params[:user][:password_confirmation]
+        @user.name = params[:user].permit![:name]
+        @user.email = params[:user].permit![:email]
+        @user.password = params[:user].permit![:password]
+        @user.password_confirmation = params[:user].permit![:password_confirmation]
         
         if @user.save
             flash[:notice] = "Welcome to Bloccit #{@user.name}!"
