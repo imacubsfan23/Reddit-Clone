b4458ce12d691d2c11d383823a921a67b8f2d87b
Tim D <imacubsfan23@gmail.com>
2017-11-14 21:10:49 +0000

    checkpoint 30 favorites completed

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ source 'https://rubygems.org'
  gem 'bootstrap-sass'
  
  gem 'bcrypt'
+ 
+ gem 'figaro', '1.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    
     before_save { self.email = email.downcase if email.present? }
     before_save { self.role ||= :member }
+    
     validates :name, 
               length: { minimum: 1, maximum: 100 }, 
               presence: true
@@ -24,4 +27,8 @@ class User < ApplicationRecord
     has_secure_password
     
     enum role: [:member, :admin]
+    
+    def favorite_for(post)
+        favorites.where(post_id: post.id).first
+    end
 end
